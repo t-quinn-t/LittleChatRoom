a49554cea8be52bda207b689fcd06815cd7561c1
breadonmars <qintutao@gmail.com>
2021-07-19 22:04:24 -0700

    added db config in application propreties

---
+++ server/src/main/java/com/server/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/test-connection")
+    public String testConnection() {
+        return "Successfully connected";
+    }
+}
