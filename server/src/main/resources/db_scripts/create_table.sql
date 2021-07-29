d65b425351e7d8e09e72e66cfb353207a5fc7937
quinn <qintutao@gmail.com>
2021-07-28 20:44:52 -0700

    added new chatroom tables to create_table.sql

--- server/src/main/resources/db_scripts/create_table.sql
+++ server/src/main/resources/db_scripts/create_table.sql
@@ -1,5 +1,10 @@
+-- we probably need to support multiple chatroom 
+create table if not exists chatrooms (
+	room_id serial primary key not null,
+	room_name varchar(50) not null
+);
 
-create table if not exists usr (
+create table if not exists users (
 	uid serial primary key not null,
 	uname varchar(50) not null,
 	registered_time timestamp with time zone not null default now(),
@@ -17,15 +22,25 @@ $$
 	end;
 $$ language plpgsql;
 
-create trigger update_user
-	before update on usr
+create trigger update_users
+	before update on users
 	for each row 
 	execute procedure update_user_trigger();
 
-create table if not exists mgs (
-	mid serial primary key not null,
+create table if not exists messages (
+	message_id serial primary key not null,
 	from_user bigint not null,
-	msg_content varchar(256) not null,
+	from_room bigint not null,
+	message_content varchar(256) not null,
 	sent_time timestamp with time zone not null default now(),
-	foreign key(from_user) references usr(uid) 
-)
+	foreign key(from_user) references usr(uid),
+	foreign key(from_room) references chatrooms(room_id)
+);
+
+create table if not exists user_registration (
+	registration_id serial primary key not null,
+	room_id_fk bigint not null,
+	user_id_fk bigint not null,
+	foreign key(room_id_fk) references chatrooms(room_id),
+	foreign key(user_id_fk) references users(uid)
+);
