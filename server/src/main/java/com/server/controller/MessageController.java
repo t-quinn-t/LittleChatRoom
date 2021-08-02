f7c8b382e78f71b199940702d9fc2d7491706cb8
quinn <qintutao@gmail.com>
2021-08-02 12:00:16 -0700

    added message controller

---
+++ server/src/main/java/com/server/controller/MessageController.java
@@ -0,0 +1,12 @@
+package com.server.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Qintu (Quinn) Tao
+ * @date: 2021-08-02 11:57 a.m.
+ */
+@Controller
+public class MessageController {
+    
+}
