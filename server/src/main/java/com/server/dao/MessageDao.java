346ddbba71b1f822e93d49d66e2504f4e68598e4
Quinn <qintutao@gmail.com>
2021-12-16 21:00:53 -0800

    refactor: reframe MessageDao to use new DB schema

--- server/src/main/java/com/server/dao/MessageDao.java
+++ server/src/main/java/com/server/dao/MessageDao.java
@@ -1,18 +1,16 @@
 package com.server.dao;
+/**
+ * @author Quinn Tao
+ */
 
 import com.server.model.Message;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-/**
- * @author Qintu (Quinn) Tao
- * @date: 2021-08-02 12:13 p.m.
- */
 @Repository
 public interface MessageDao {
-    public Message save(Message message);
-    public void delete(Message message);
-    public Message getMessageByMessageId(Long messageId);
-    public List<Message> getMessagesByRoomId(Long roomId);
+    public Message          save(Message message);
+    public void             delete(Message message);
+    public Message          getMessageByMessageId(Long messageId);
+    public List<Message>    getMessagesByRoomId(Long roomId);
 }
